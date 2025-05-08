13dee870aa7f0775af116da8938ff056ce8e4192
AdrianRamirez23 <74911889+AdrianRamirez23@users.noreply.github.com>
2025-05-08 14:53:34 -0500

    Update mongodb_comandos.js

--- mongodb_comandos.js
+++ mongodb_comandos.js
@@ -1,25 +1,33 @@
-/* Inserción masiva de registros simulados */
+// Inserción masiva
 for (let i = 1; i <= 100; i++) {
   const tipos = ["temperatura", "humedad", "ruido", "co2"];
   const ciudades = ["Bogotá", "Medellín", "Cali", "Barranquilla", "Cartagena"];
   const tipo = tipos[Math.floor(Math.random() * tipos.length)];
   const ciudad = ciudades[Math.floor(Math.random() * ciudades.length)];
   db.registros.insertOne({
-    sensor_id: `sensor${i}`,
+    sensor_id: `SEN-00${i}`,
     tipo: tipo,
     valor: Math.random() * 100,
-    unidad: tipo === "temperatura" ? "°C" : tipo === "co2" ? "ppm" : "%",
+    unidad: tipo === "temperatura" ? "C" : tipo === "co2" ? "ppm" : "%",
     timestamp: new Date(),
     ciudad: ciudad
   });
 }
+// Consultas básicas
+// 1. Selección
+> db.registros.find({ tipo: "temperatura" })
+// 2. Actualización
+> db.registros.updateOne({ sensor_id: "SEN-005 }, { $set: { valor: 25.5 } })
+// 3. Eliminación
+> db.registros.deleteOne({ sensor_id: "SEN-100" })
 
-/* Operaciones CRUD */
-db.registros.find({ tipo: "temperatura" })
-db.registros.updateOne({ sensor_id: "sensor5" }, { $set: { valor: 25.5 } })
-db.registros.deleteOne({ sensor_id: "sensor100" })
-
-/* Agregación por ciudad */
+// 4. Agregación
 db.registros.aggregate([
-  { $group: { _id: "$ciudad", avg_valor: { $avg: "$valor" }, total: { $sum: 1 } } }
-])
+  {
+    $group: {
+      _id: "$ubicacion.ciudad",
+      avg_valor: { $avg: "$valor" },
+      total: { $sum: 1 }
+    }
+  }
+])])
